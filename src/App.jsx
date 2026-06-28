30b8870db0a67cb042f3de696f27eec8049331be
Rahul <rahul.wa47@gmail.com>
2026-06-28 20:53:46 +0530

    fetched product data and updated the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router'
 import Navbar from './components/Navbar'
 import styles from "./App.module.css"
@@ -8,6 +8,28 @@ function App() {
   const [shopData, setShopData] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        if(!response.ok) {
+          throw new Error("failed to fetch data!")
+          console.log("ssdfsdfsdf");
+          
+        }
+        const data = await response.json()
+        setShopData(data)
+        console.log(data)
+      } catch (error) {
+        setError(error.message)
+        console.log("hiiiiii")
+      }finally {
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
 
   return (
   
