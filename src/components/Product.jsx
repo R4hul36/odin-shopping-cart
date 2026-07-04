c6129cc7e7e8ca6944bfe6993dac9536c912ce89
Rahul <rahul.wa47@gmail.com>
2026-07-04 18:06:34 +0530

    added styling to product card

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,9 +5,13 @@ const Product = ({productInfo}) => {
   const {category, title, description, id, image, price, rating} = productInfo 
   return (
     <div className={styles['product-card']}>
-      <img src={image} alt={category} />
-      <h3>{title}</h3>
-      <p>{price}</p>
+      <div className={styles["product-info"]}>
+        <div className={styles["img-container"]}>
+          <img src={image} alt={category} />
+        </div>
+        <h3 title={title}>{title}</h3>
+        <p>${price}</p>
+      </div>
       <button>Add To Cart</button>
     </div>
   )
